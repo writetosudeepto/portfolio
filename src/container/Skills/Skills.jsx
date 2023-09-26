4df66bd3515fa31b3d067d70bf3843c3997067d9
writetosudeepto <writetosudeepto@gmail.com>
2023-09-27 03:37:40 +0530

    skills section fix

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -1,6 +1,8 @@
 import { Tooltip as ReactTooltip } from "react-tooltip";
+
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
+
 import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 import "./Skills.scss";
@@ -52,38 +54,29 @@ const Skills = () => {
                 <p className="bold-text">{experience.year}</p>
               </div>
               <motion.div className="app__skills-exp-works">
-                {experiences.map((experience) => (
-                  <motion.div
-                    className="app__skills-exp-item"
-                    key={experience.year}
-                  >
-                    <div className="app__skills-exp-year">
-                      <p className="bold-text">{experience.year}</p>
-                    </div>
-                    <motion.div className="app__skills-exp-works">
-                      {experience.works.map((work) => (
-                        <React.Fragment key={`${experience.year}-${work.name}`}>
-                          <motion.div
-                            whileInView={{ opacity: [0, 1] }}
-                            transition={{ duration: 0.5 }}
-                            className="app__skills-exp-work"
-                            data-tip
-                            data-for={work.name}
-                            data-tooltip-id={work.name}
-                          >
-                            <h4 className="bold-text">{work.name}</h4>
-                            <p className="p-text">{work.company}</p>
-                          </motion.div>
-                          <ReactTooltip
-                            id={work.name}
-                            className="skills-tooltip"
-                          >
-                            {work.desc}
-                          </ReactTooltip>
-                        </React.Fragment>
-                      ))}
+                {experience.works.map((work) => (
+                  <>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                      className="app__skills-exp-work"
+                      data-tip
+                      data-for={work.name}
+                      key={work.name}
+                      data-tooltip-id={work.name}
+                    >
+                      <h4 className="bold-text">{work.name}</h4>
+                      <p className="p-text">{work.company}</p>
                     </motion.div>
-                  </motion.div>
+                    <ReactTooltip
+                      id={work.name}
+                      effect="solid"
+                      arrowColor="#fff"
+                      className="skills-tooltip"
+                    >
+                      {work.desc}
+                    </ReactTooltip>
+                  </>
                 ))}
               </motion.div>
             </motion.div>
