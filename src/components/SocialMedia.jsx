fb71562ef0a5f904b06a7b2e8d6b1545c0379ef4
writetosudeepto <writetosudeepto@gmail.com>
2023-09-29 02:22:54 +0530

    social-media links added

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,17 +1,64 @@
 import React from "react";
 import { BsTwitter, BsInstagram } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
+import { SiHackerrank, SiTableau } from "react-icons/si";
+import { PiGithubLogoBold } from "react-icons/pi";
+import { TbBrandLinkedin } from "react-icons/tb";
 
 const SocialMedia = () => {
+  const handleIconClick = (url) => {
+    window.open(url, "_blank"); // Opens the URL in a new tab
+  };
   return (
     <div className="app__social">
+      <div
+        onClick={() => handleIconClick("https://github.com/writetosudeepto")}
+      >
+        <PiGithubLogoBold />
+      </div>
+      <div
+        onClick={() =>
+          handleIconClick("https://www.linkedin.com/in/writetosudeepto")
+        }
+      >
+        <TbBrandLinkedin />
+      </div>
       <div>
+        <SiHackerrank
+          onClick={() =>
+            handleIconClick("https://www.hackerrank.com/writetosudeepto")
+          }
+        />
+      </div>
+
+      <div
+        onClick={() =>
+          handleIconClick(
+            "https://public.tableau.com/app/profile/sudipta.das5968"
+          )
+        }
+      >
+        <SiTableau />
+      </div>
+      <div
+        onClick={() => handleIconClick("https://twitter.com/writetosudeepto")}
+      >
         <BsTwitter />
       </div>
-      <div>
+      <div
+        onClick={() =>
+          handleIconClick("https://www.facebook.com/thealwaysuncertain")
+        }
+      >
         <FaFacebookF />
       </div>
-      <div>
+      <div
+        onClick={() =>
+          handleIconClick(
+            "https://www.instagram.com/the_principle_of_uncertainty"
+          )
+        }
+      >
         <BsInstagram />
       </div>
     </div>
